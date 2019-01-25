ca6cb36873698959bb07a0dc98f74b2342fe9bf5
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2019-01-25 17:49:06 -0500

    Add PitchTypes

---
+++ MlbStatsAcquisition.Processor/Feeds/PitchTypesFeed.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MlbStatsAcquisition.Processor.Feeds
+{
+	public class PitchTypesFeed
+	{
+		public static string GetFeedUrl()
+		{
+			return "http://statsapi.mlb.com/api/v1/pitchTypes";
+		}
+
+		public static List<PitchTypesFeed> FromJson(string json) => JsonConvert.DeserializeObject<List<PitchTypesFeed>>(json, Converter.Settings);
+
+		[JsonProperty("code")]
+		public string Code { get; set; }
+
+		[JsonProperty("description")]
+		public string Description { get; set; }
+	}
+
+	public static partial class Serialize
+	{
+		public static string ToJson(this List<PitchTypesFeed> self) => JsonConvert.SerializeObject(self, Converter.Settings);
+	}
+}
---
+++ MlbStatsAcquisition.Processor/Processors/PitchTypesProcessor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MlbStatsAcquisition.Processor.Processors
+{
+	public class PitchTypesProcessor : IProcessor
+	{
+		public void Run()
+		{
+			List<Feeds.PitchTypesFeed> feed;
+			using (var client = new WebClient())
+			{
+				var url = Feeds.PitchTypesFeed.GetFeedUrl();
+				var rawJson = client.DownloadString(url);
+				feed = Feeds.PitchTypesFeed.FromJson(rawJson);
+			}
+			using (var context = new Model.MlbStatsContext())
+			{
+				var dbPitchTypes = context.PitchTypes.ToDictionary(x => x.Code);
+				foreach (var feedPitchType in feed)
+				{
+					if (!dbPitchTypes.TryGetValue(feedPitchType.Code, out Model.PitchType dbPitchType))
+					{
+						dbPitchType = new Model.PitchType
+						{
+							Code = feedPitchType.Code,
+							Description = feedPitchType.Description
+						};
+						dbPitchTypes.Add(feedPitchType.Code, dbPitchType);
+						context.PitchTypes.Add(dbPitchType);
+					}
+					else
+					{
+						if (!string.Equals(dbPitchType.Description, feedPitchType.Description, StringComparison.InvariantCultureIgnoreCase))
+						{
+							dbPitchType.Description = feedPitchType.Description;
+						}
+					}
+				}
+				context.SaveChanges();
+			}
+		}
+	}
+}
